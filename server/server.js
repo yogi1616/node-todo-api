843049a48ea401ac45f58eaa3e7185c9a74cb42d
yogi1616 <yogesh_barot07@yahoo.com>
2019-02-21 15:52:57 +0530

    Add GET todos/:id

--- server/server.js
+++ server/server.js
@@ -1,3 +1,5 @@
+var {ObjectID} = require('mongodb');
+
 var express = require('express');
 var bodyParser = require('body-parser');
 
@@ -28,6 +30,22 @@ app.get('/todos', (req, res) => {
     res.status(400).send(e);
   });
 });
+
+app.get('/todos/:id',(req,res)=>{
+  var id = req.params.id; 
+  if(!ObjectID.isValid(id)){
+    return res.status(404).send();
+}   
+  Todo.findById(id).then((todo)=>{
+    if(!todo){
+      return  res.status(404).send();
+    }
+      res.send({todo});
+}).catch((e)=>{
+  res.status(400).send();
+});
+});
+
 
 app.listen(3000, () => {
   console.log('Started on port 3000');
